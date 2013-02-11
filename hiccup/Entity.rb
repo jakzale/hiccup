e21e7bc432265d4b989a843d7c85271b8f40cab1
Jakub Z <jakzale@gmail.com>
2013-02-11 23:05:10 +0100

    Clean up testing

--- hiccup/Entity.rb
+++ hiccup/Entity.rb
@@ -5,7 +5,23 @@ class Entity
     attr_accessor :description
     def initialize(description)
         @description = description
+        @links = {}
     end
 
-    
+    def [](id)
+        @links[id]
+    end
+
+    def link(tag, entity)
+        @links[tag] = entity
+    end
+
+    def links
+        @links.keys
+    end
+
+    def unlink(key)
+        @links.delete key
+        return
+    end
 end
--- spec/Entity_spec.rb
+++ spec/Entity_spec.rb
@@ -3,9 +3,35 @@ require 'spec_helper'
 require 'Entity'
 
 describe Entity do
+    # Common logic for the entity tests
+
     it "should initialize properly with a description" do
         description = "Some description"
         entity = Entity.new description
         entity.description.should eq(description)
+    end
+
+    describe "Linking" do
+        before do
+            @boo = Entity.new "Boo"
+            @bam = Entity.new "Bam"
+        end
+
+        it "should link" do
+            # boo = Entity.new "Boo"
+            # bam = Entity.new "Bam"
+            @boo.link "bam", @bam
+            @boo.links.include?('bam').should be_true
+            @boo["bam"].should eq(@bam)
+        end
+
+        it "should unlink" do
+            @boo.links.should be_empty
+            @boo.unlink "Bam"
+            @boo.link :bam, @bam
+            @boo.links.include?(:bam).should be_true
+            @boo.unlink :bam
+            @boo.links.should be_empty
+        end
     end
 end
